48639a1ea70efed168e452b21577262015c17ea1
Simon Shields <keepcalm444@gmail.com>
2016-01-10 10:59:10 +1100

    add template demo to server, fix some bugs in template engine

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from tornado.ncss import Server, ncssbook_log
 
+from engine.template import render
+
 def index_handler(response):
   with open("indextest.html") as file:
     response.write(file.read())
@@ -8,7 +10,11 @@ def profile_handler(response, user_id):
     with open("indextest.html") as file:
       response.write("Profile ID: " + user_id + file.read())
 
+def template_demo(response):
+    response.write(render("test.html", {}))
+
 server = Server()
 server.register(r"/", index_handler)
 server.register(r"/profile/(\d+)/", profile_handler)
+server.register(r"/template", template_demo)
 server.run()
