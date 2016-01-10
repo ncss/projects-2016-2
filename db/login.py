611dbe607f290a847031cfac7b102e153da24495
Alex <alex.mcclung@hotmail.com>
2016-01-10 12:43:27 +1100

    added login, register and delete user funcionallity to login.py

--- db/login.py
+++ db/login.py
@@ -2,13 +2,44 @@ import sqlite3
 
 conn = sqlite3.connect('ncssbook.db')
 
-def login(username, password):
-    cur = conn.execute('''
-    SELECT id
-    FROM users
-    WHERE user=? AND pass=?
-    ''', (username, password))
-    row = cur.fetchone()
-    user_id = None if row is None else row['id']
-    conn.commit()
-    return user_id
+
+class User:
+    userCount = 0
+    
+    def __init__(self, username, password, *, user_id=None, fname="", lname="", email="", dob="", postcode="", country_code="", user_permissions="", signup_timestamp="", image=""):
+       self.username = username
+       self.password = password
+       self.user_id = user_id
+       self.fname = fname
+       self.lname = lname
+       self.email = email
+       self.dob = dob
+       self.postcode = postcode
+       self.country_code = country_code
+       self.user_permissions = user_permissions
+       self.signup_timestamp = signup_timestamp
+       self.image = image
+
+    def login(self):
+        cur = conn.execute('''
+        SELECT *
+        FROM users
+        WHERE username=? AND password=?
+        ''', (self.username, self.password))
+        row = cur.fetchone()
+        conn.commit()
+        return row
+        conn.commit()
+
+
+    def register(self):
+        cur = conn.execute('''
+        INSERT into users VALUES(?,?,?,?,?,?,?,?,?,?)''', (username, password, fname, lname, email, dob, postcode, country_code, user_permissions, signup_timestamp, image))
+        
+        return cur.lastrowid
+
+   
+    def delete(self):
+        cur = conn.execute('''
+        DELETE FROM users WHERE username = ?''', (self.username))
+        return user_id
