94df8b32c5d4a4e69393787adc04009c1b6d058a
The True Overlord <chokstar@chokstarproductions.com>
2016-01-10 12:46:00 +1100

    Revert "Still working" - trying to get Server back after git merge mess.
    This reverts commit 98249bfc09f2909c041bf8e01f369cfefaea0b8f.

--- server.py
+++ server.py
@@ -1,14 +1,40 @@
 from tornado.ncss import Server, ncssbook_log
 
-def index_handler(response):
-  with open("indextest.html") as file:
+def landing_handler(response):
+  with open("templates/landing.html") as file:
+    response.write(file.read())
+
+def home_handler(response):
+  with open("templates/home.html") as file:
+    response.write(file.read())
+	
+def register_handler(response):
+  with open("templates/register.html") as file:
     response.write(file.read())
 
 def profile_handler(response, user_id):
-    with open("indextest.html") as file:
+    with open("templates/profile.html") as file:
       response.write("Profile ID: " + user_id + file.read())
+	  
+def input_handler(response):
+  with open("templates/input.html") as file:
+    response.write(file.read())
+	
+def updateprofile_handler(response):
+  with open("templates/updateprofile.html") as file:
+    response.write(file.read())
 
+def search_handler(response):
+  with open("templates/search.html") as file:
+    response.write(file.read())
+	
 server = Server()
-server.register(r"/", index_handler)
+server.register(r"/", landing_handler)
+server.register(r"/home/", home_handler)
+server.register(r"/register/", register_handler)
 server.register(r"/profile/(\d+)/", profile_handler)
+server.register(r"/input/", input_handler)
+server.register(r"/updateprofile/", updateprofile_handler)
+server.register(r"/search/", search_handler)
+
 server.run()
