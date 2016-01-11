560612b1ced708d540f0107b57f5406bb5913aa4
sarahhercock <sarah.hercock@gmail.com>
2016-01-11 20:57:57 +1100

    fixed server and activity input form

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from tornado.ncss import Server, ncssbook_log
-from activities import ActivityInputHandler, activity_dict
+from activities import ActivityInputHandler
 from engine.template import render
 
 
@@ -8,16 +8,21 @@ def landing_handler(response):
 
 def home_handler(response):
     response.write(render("feed.html", {'a': 'B'}))
-	
+    
 def register_handler(response):
     response.write(render("register.html", {'a': 'B'}))
 
 def profile_handler(response, user_id):
     response.write(render("profile.html", {'user_id': 51}))
 
-def input_handler(response):
-    aih = ActivityInputHandler(activity_dict)
+def input_handler_get(response):
+    aih = ActivityInputHandler()
     response.write(render("input_activity.html", aih.get_template_data()))
+    
+def input_handler_post(response):
+    aih = ActivityInputHandler()
+    aih.load_activity_data(response)
+    
 
 def updateprofile_handler(response):
   response.write(render("update_profile.html", {'a': 'B'}))
@@ -30,14 +35,14 @@ def search_handler(response):
 
 def login_handler(response):
   response.write(render("login.html", {'a': 'B'}))
-	
+    
 server = Server()
 
 server.register(r"/", landing_handler)
 server.register(r"/home/", home_handler)
 server.register(r"/register/", register_handler)
 server.register(r"/profile/(\d+)/", profile_handler)
-server.register(r"/input/", input_handler)
+server.register(r"/input/", input_handler_get, post=input_handler_post)
 server.register(r"/updateprofile/", updateprofile_handler)
 server.register(r"/search/", search_handler)
 server.register(r"/template/", template_demo)
