269d8e9ffb2fae0448e0aaa3d285b2a51b822131
Alex <alex.mcclung@hotmail.com>
2016-01-11 20:05:57 +1100

    changed email to user id

--- login_backend.py
+++ login_backend.py
@@ -1,11 +1,11 @@
 import db.login as db
 
-def set_login_cookie(response, email):
-    response.set_secure_cookie("email", email) 
+def set_login_cookie(response, user_id):
+    response.set_secure_cookie("user_id", str(user_id)) 
 
 def get_login_cookie(response):
-    email = response.get_secure_cookie("email")
-    return email
+    user_id = int(response.get_secure_cookie("user_id"))
+    return user_id
 
 def login(response,email,password):
     '''
@@ -13,16 +13,16 @@ def login(response,email,password):
     -Call db function
     -Cookies
     '''
-    cookie_email = get_login_cookie(response)
-    if cookie_email is not None:
-        user = db.User
-        user.email = cookie_email
+    cookie_user_id = get_login_cookie(response)
+    if cookie_user_id is not None:
+        user = db.User()
+        user.user_id = cookie_user_id
         return user.load()
     else:
         user = db.User.login(email, password)
         if user is None:
             return None
-        set_login_cookie(response, user.email)
+        set_login_cookie(response, user.user_id)
         return user
 
 
